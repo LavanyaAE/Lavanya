47815b4b573cde2c24414d18c51a9d099b2ac22d
LavanyaAE <aelavanya.xworkz@gmail.com>
2023-06-27 14:30:50 +0530

    First program

---
+++ Emails.java
@@ -0,0 +1,40 @@
+class Emails
+{
+	public static void main(String[] args)
+	{
+		String email1="aelavanya@gmail.com";
+		String email2="aepushpa@gmail.com";
+		String email3="abc@gmail.com";
+		String email4="ghj@gmail.com";
+		String email5="sad123@gmail.com";
+	    String email6="happy@gmail.com";
+		String email7="super@gmail.com";
+		String email8="duper@gmail.com";
+		String email9="bumper@gmail.com";
+		String email10="nothing@gmail.com";
+		String email11="server@gmail.com";
+		String email12="driver@gmail.com";
+		String email13="shopper@gmail.com";
+		String email14="abcohv@gmail.com";
+		String email15="ghjj@gmail.com";
+		String[] emailID={email1,email2,email3,email4,email5,email6,email7,email8,email9,email10,email11,email12,email13,email14};
+		System.out.println("Number of email IDs:"+emailID.length);
+		System.out.println("And the IDs are:");
+		System.out.println(emailID[0]);
+		System.out.println(emailID[1]);
+		System.out.println(emailID[2]);
+		System.out.println(emailID[3]);
+		System.out.println(emailID[4]);
+		System.out.println(emailID[5]);
+		System.out.println(emailID[6]);
+		System.out.println(emailID[7]);
+		System.out.println(emailID[8]);
+		System.out.println(emailID[9]);
+		System.out.println(emailID[10]);
+		System.out.println(emailID[11]);
+		System.out.println(emailID[12]);
+		System.out.println(emailID[13]);
+		System.out.println(emailID[14]);
+		
+	}
+}
