ab5b170210e87c8076f2d970e974991497f217f0
LavanyaAE <aelavanya.xworkz@gmail.com>
2023-07-06 09:26:42 +0530

    Taskkk

---
+++ TaskJuly5/InfosysRunner.java
@@ -0,0 +1,11 @@
+class InfosysRunner
+{
+	public static void main(String[] infosys)
+	{
+		Infosys detail = new Infosys("Narayan Murthi","Sudha Narayan Murthi",1981,35186);
+		System.out.println("FounderName :"+detail.founderName);
+		System.out.println("FounderWifeName :"+detail.founderWifeName);
+		System.out.println("Founded :"+detail.founded);
+		System.out.println("TotalEmployees :"+detail.totalEmployees);
+	}
+}	
